184e0e865b28d9bc78bb04ccc96b6936b2cf43b2
ZubkovAC <3y6kob90@gmail.com>
2021-07-13 22:53:38 +0300

    type HOC

--- src/utils/Redirect.tsx
+++ src/utils/Redirect.tsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React, {ReactNode} from "react";
 import {RoutePath} from "../App";
 import {useSelector} from "react-redux";
 import {AppStateType} from "../state/store";
 import { Redirect } from "react-router-dom";
 
 type AuthorizedPropsType = {
-    children:any
+    children: ReactNode
 }
 
 export const Authorized = (props:AuthorizedPropsType) =>{
 
     const authorized = useSelector<AppStateType, boolean>(state => state.tasks.authorized)
-
     if (!authorized) return <Redirect to={RoutePath.HOME_RAGE}/>;
     return (
         <div >
